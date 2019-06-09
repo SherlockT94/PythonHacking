e395a683ab222b91c1c9c954a431e6a0e5f17b2f
root <root@localhost.localdomain>
2019-06-09 15:47:26 +1000

    finishing a basic python script to change MAC address

--- MACChanger/mac_changer.py
+++ MACChanger/mac_changer.py
@@ -1,3 +1,22 @@
 import subprocess
+import argparse
 
-subprocess.call("ifconfig", shell=True)
+def getArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Interfacef to change its MAC address")
+    parser.add_argument("-m", "--mac", dest="newMAC", help="New MAC Address")
+    args = parser.parse_args()
+    if not args.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    if not args.newMAC:
+        parser.error("[-] Please specify an new MAC address, use --help for more info")
+    return args
+
+def changeMAC(interface, newMAC):
+    print("[+] Changing MAC address for " + interface + " to " + newMAC)
+    subprocess.call(["ifconfig", interface, "down"])#for security reason, use + to concatenate String may cause code injection
+    subprocess.call(["ifconfig", interface, "hw", "ether", newMAC])
+    subprocess.call(["ifconfig", interface, "up"])
+
+args = getArguments()
+changeMAC(args.interface, args.newMAC)
