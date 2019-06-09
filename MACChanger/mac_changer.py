32dca8b1383fc902afc1fef415c7551115f07c01
root <root@localhost.localdomain>
2019-06-09 17:44:08 +1000

    add the check function to see whether change the MAC address successfully

--- MACChanger/mac_changer.py
+++ MACChanger/mac_changer.py
@@ -1,9 +1,10 @@
-import subprocess
-import argparse
+import subprocess 
+import argparse 
+import re
 
-def getArguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interface", dest="interface", help="Interfacef to change its MAC address")
+def getArguments(): 
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("-i", "--interface", dest="interface", help="Interfacef to change its MAC address") 
     parser.add_argument("-m", "--mac", dest="newMAC", help="New MAC Address")
     args = parser.parse_args()
     if not args.interface:
@@ -18,5 +19,21 @@ def changeMAC(interface, newMAC):
     subprocess.call(["ifconfig", interface, "hw", "ether", newMAC])
     subprocess.call(["ifconfig", interface, "up"])
 
+def getCurrentMAC(interface):
+    ifResult = subprocess.check_output(["ifconfig", interface])
+    extractMAC = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifResult)
+    if extractMAC:
+        return extractMAC.group(0)
+    else:
+        print("[-] Could not read MAC Address.")
+
 args = getArguments()
+currentMAC = getCurrentMAC(args.interface)
+print("Current MAC = " + str(currentMAC))
+
 changeMAC(args.interface, args.newMAC)
+currentMAC = getCurrentMAC(args.interface)
+if currentMAC == args.newMAC:
+    print("[+] MAC address was successfully changed to " + currentMAC)
+else:
+    print("[-] MAC address did not get changed MAC address did not get changed.")
