1c86876fbbed7581cff67347ac060da94c17d0df
root <root@localhost.localdomain>
2019-07-20 07:55:55 -0400

    net_cut.py

--- DNSSpoofer/net_cut.py
+++ DNSSpoofer/net_cut.py
@@ -1,1 +1,12 @@
 # netfilterqueue has some issues with pyhton3, so using python2 to run this script
+import netfilterqueue
+
+def process_packet(packet):
+    print(packet)
+    packet.accept()#forward package
+    packet.drop()#drop package
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)#process_packet is a function to handle every packet trapped
+queue.run()
+
